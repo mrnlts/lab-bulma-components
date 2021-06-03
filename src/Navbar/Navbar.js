479d50dd8c0dbb222966c117093eac74e8cb9a0f
marina <marinaaltesmagret@gmail.com>
2021-06-03 18:10:26 +0200

    Iteration 1

---
+++ src/Navbar/Navbar.js
@@ -0,0 +1,32 @@
+import 'bulma/css/bulma.css';
+import React, { Component } from 'react';
+import './Navbar.css';
+
+class Navbar extends Component {
+    render() {
+        return (
+            <nav class="navbar-container is-fixed-top">
+                <div class="navbar-to-left">
+                    <img src="https://bulma.io/images/bulma-logo.png" alt="Bulma: a modern CSS framework based on Flexbox" width="112" height="28"/>
+                    <p>Home</p>
+                </div>
+                
+                <div class="navbar-to-right">
+                    <p class="control">
+                        <a class="button blue" href="https://github.com/jgthms/bulma/releases/download/0.9.2/bulma-0.9.2.zip">
+                        <span>Login</span>
+                        </a>
+                    </p>
+                    <p class="control">
+                        <a class="button is-primary" href="https://github.com/jgthms/bulma/releases/download/0.9.2/bulma-0.9.2.zip">
+                        <span>Signup</span>
+                        </a>
+                    </p>
+                </div>
+            </nav>
+        )
+    }
+}
+
+
+export default Navbar;
